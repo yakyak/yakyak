fab6dbbd34b18add2f24a8438a6ec162005d8848
André Veríssimo <afsverissimo@gmail.com>
2016-06-29 17:21:45 +0100

    moved os deployments to function calls
    this way it is possible to build only one platform
     in a custom deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,47 +19,61 @@ for PLATFORM in ${PLATFORMS[*]}; do
     unzip -o electron-v$ELECTRON_VERSION-$PLATFORM.zip -d $PLATFORM
 done
 
-cd darwin-x64
-mv Electron.app YakYak.app
-defaults write $(pwd)/YakYak.app/Contents/Info.plist CFBundleDisplayName -string "YakYak"
-defaults write $(pwd)/YakYak.app/Contents/Info.plist CFBundleExecutable -string "YakYak"
-defaults write $(pwd)/YakYak.app/Contents/Info.plist CFBundleIdentifier -string "com.github.yakyak"
-defaults write $(pwd)/YakYak.app/Contents/Info.plist CFBundleName -string "YakYak"
-defaults write $(pwd)/YakYak.app/Contents/Info.plist CFBundleVersion -string "$VERSION"
-plutil -convert xml1 $(pwd)/YakYak.app/Contents/Info.plist
-mv YakYak.app/Contents/MacOS/Electron YakYak.app/Contents/MacOS/YakYak
-cp -R ../../app YakYak.app/Contents/Resources/app
-cp ../../src/icons/icon.icns YakYak.app/Contents/Resources/electron.icns
-zip -r -y -X ../yakyak-osx.app.zip YakYak.app
-# ditto -c -k --rsrc --extattr --keepParent YakYak.app ../yakyak-osx.app.zip
-cd ..
+build_max() {
+  cd darwin-x64
+  mv Electron.app YakYak.app
+  defaults write $(pwd)/YakYak.app/Contents/Info.plist CFBundleDisplayName -string "YakYak"
+  defaults write $(pwd)/YakYak.app/Contents/Info.plist CFBundleExecutable -string "YakYak"
+  defaults write $(pwd)/YakYak.app/Contents/Info.plist CFBundleIdentifier -string "com.github.yakyak"
+  defaults write $(pwd)/YakYak.app/Contents/Info.plist CFBundleName -string "YakYak"
+  defaults write $(pwd)/YakYak.app/Contents/Info.plist CFBundleVersion -string "$VERSION"
+  plutil -convert xml1 $(pwd)/YakYak.app/Contents/Info.plist
+  mv YakYak.app/Contents/MacOS/Electron YakYak.app/Contents/MacOS/YakYak
+  cp -R ../../app YakYak.app/Contents/Resources/app
+  cp ../../src/icons/icon.icns YakYak.app/Contents/Resources/electron.icns
+  zip -r -y -X ../yakyak-osx.app.zip YakYak.app
+  # ditto -c -k --rsrc --extattr --keepParent YakYak.app ../yakyak-osx.app.zip
+  cd ..
+}
 
-cd win32-ia32
-mv electron.exe yakyak.exe
-cp -R ../../app resources/app
-cd ..
-zip -r yakyak-win32-ia32.zip win32-ia32
-# ditto -c -k --rsrc --extattr --keepParent win32-ia32 yakyak-win32-ia32.zip
+build_win32_ia32 () {
+  cd win32-ia32
+  mv electron.exe yakyak.exe
+  cp -R ../../app resources/app
+  cd ..
+  zip -r yakyak-win32-ia32.zip win32-ia32
+  # ditto -c -k --rsrc --extattr --keepParent win32-ia32 yakyak-win32-ia32.zip
+}
 
+build_win32_x64 () {
+  cd win32-x64
+  mv electron.exe yakyak.exe
+  cp -R ../../app resources/app
+  cd ..
+  zip -r yakyak-win32-x64.zip win32-x64
+  # ditto -c -k --rsrc --extattr --keepParent win32-x64 yakyak-win32-x64.zip
+}
 
-cd win32-x64
-mv electron.exe yakyak.exe
-cp -R ../../app resources/app
-cd ..
-zip -r yakyak-win32-x64.zip win32-x64
-# ditto -c -k --rsrc --extattr --keepParent win32-x64 yakyak-win32-x64.zip
+build_linux_ia32 () {
+  cd linux-ia32
+  mv electron yakyak
+  cp -R ../../app resources/app
+  cd ..
+  zip -r yakyak-linux-ia32.zip linux-ia32
+  # ditto -c -k --rsrc --extattr --keepParent linux-ia32 yakyak-linux-ia32.zip
+}
 
-cd linux-ia32
-mv electron yakyak
-cp -R ../../app resources/app
-cd ..
-zip -r yakyak-linux-ia32.zip linux-ia32
-# ditto -c -k --rsrc --extattr --keepParent linux-ia32 yakyak-linux-ia32.zip
+build_linux_x64 () {
+  cd linux-x64
+  mv electron yakyak
+  cp -R ../../app resources/app
+  cd ..
+  zip -r yakyak-linux-x64.zip linux-x64
+  # ditto -c -k --rsrc --extattr --keepParent linux-x64 yakyak-linux-x64.zip
+  cd ..
+}
 
-cd linux-x64
-mv electron yakyak
-cp -R ../../app resources/app
-cd ..
-zip -r yakyak-linux-x64.zip linux-x64
-# ditto -c -k --rsrc --extattr --keepParent linux-x64 yakyak-linux-x64.zip
-cd ..
+build_win32_ia32
+build_win32_x64
+build_linux_ia32
+build_linux_x64
