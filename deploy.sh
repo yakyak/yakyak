f161b438ad722ebc94c09849b845d1aa82048050
André Veríssimo <afsverissimo@gmail.com>
2016-06-29 17:52:45 +0100

    deploy.sh accepts arguments to build only certain platforms

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,59 @@
 #!/bin/bash
 
+
+# Use -gt 1 to consume two arguments per pass in the loop (e.g. each
+# argument has a corresponding value to go with it).
+# Use -gt 0 to consume one or more arguments per pass in the loop (e.g.
+# some arguments don't have a corresponding value to go with it such
+# as in the --default example).
+# note: if this is set to -gt 0 the /etc/hosts part is not recognized ( may be a bug )
+
+DARWIN_X64="darwin-x64"
+LINUX_X64="linux-x64"
+LINUX_IA32="linux-ia32"
+WIN32_IA32="win32-ia32"
+WIN32_X64="win32-x64"
+
+ALLPLATFORMS=($DARWIN_X64 $LINUX_X64 $LINUX_IA32 $WIN32_IA32 $WIN32_X64)
+PLATFORMS=()
+
+if [[ $# -eq 0 ]]; then
+  PLATFORMS=${ALLPLATFORMS[*]}
+else
+  while [[ $# -gt 0 ]]
+  do
+    key="$1"
+
+    case $key in
+      --darwin-x64)
+        PLATFORMS=("${PLATFORMS[@]}" $DARWIN_X64)
+        ;;
+      --linux-x64)
+        PLATFORMS=("${PLATFORMS[@]}" $LINUX_X64)
+        ;;
+      --linux-ia32)
+        PLATFORMS=("${PLATFORMS[@]}" $LINUX_IA32)
+        ;;
+      --win32-x64)
+        PLATFORMS=("${PLATFORMS[@]}" $WIN32_X64)
+        ;;
+      --win32-ia32)
+        PLATFORMS=("${PLATFORMS[@]}" $WIN32_IA32)
+        ;;
+      --all)
+        PLATFORMS=${ALLPLATFORMS[*]}
+        break
+        ;;
+      *)
+        # unknown option
+        echo "unknown: $key"
+        exit
+        ;;
+    esac
+    shift # past argument or value
+  done
+fi
+#
 for dep in curl unzip sed; do
   echo "checking dependency... $dep"
   test ! $(which $dep) && echo "ERROR: missing $dep" && exit 1
@@ -7,20 +61,19 @@ done
 
 ELECTRON_VERSION=$(npm list --depth=0 |grep electron-prebuilt | cut -f2 -d@)
 VERSION=$(node -e "console.log(require('./package').version)")
-PLATFORMS=("darwin-x64" "linux-ia32" "linux-x64" "win32-ia32" "win32-x64")
 
 mkdir -p dist
 cd dist
 for PLATFORM in ${PLATFORMS[*]}; do
-    rm -rf $PLATFORM
-    echo "https://github.com/atom/electron/releases/download/v$ELECTRON_VERSION/electron-v$ELECTRON_VERSION-$PLATFORM.zip"
-    test ! -f electron-v$ELECTRON_VERSION-$PLATFORM.zip && \
+  rm -rf $PLATFORM
+  echo "https://github.com/atom/electron/releases/download/v$ELECTRON_VERSION/electron-v$ELECTRON_VERSION-$PLATFORM.zip"
+  test ! -f electron-v$ELECTRON_VERSION-$PLATFORM.zip && \
     curl -LO https://github.com/atom/electron/releases/download/v$ELECTRON_VERSION/electron-v$ELECTRON_VERSION-$PLATFORM.zip
-    unzip -o electron-v$ELECTRON_VERSION-$PLATFORM.zip -d $PLATFORM
+  unzip -o electron-v$ELECTRON_VERSION-$PLATFORM.zip -d $PLATFORM
 done
 
-build_max() {
-  cd darwin-x64
+build_darwin_x64() {
+  cd $DARWIN_X64
   mv Electron.app YakYak.app
   defaults write $(pwd)/YakYak.app/Contents/Info.plist CFBundleDisplayName -string "YakYak"
   defaults write $(pwd)/YakYak.app/Contents/Info.plist CFBundleExecutable -string "YakYak"
@@ -37,7 +90,7 @@ build_max() {
 }
 
 build_win32_ia32 () {
-  cd win32-ia32
+  cd $WIN32_IA32
   mv electron.exe yakyak.exe
   cp -R ../../app resources/app
   cd ..
@@ -46,7 +99,7 @@ build_win32_ia32 () {
 }
 
 build_win32_x64 () {
-  cd win32-x64
+  cd $WIN32_X64
   mv electron.exe yakyak.exe
   cp -R ../../app resources/app
   cd ..
@@ -55,7 +108,7 @@ build_win32_x64 () {
 }
 
 build_linux_ia32 () {
-  cd linux-ia32
+  cd $LINUX_IA32
   mv electron yakyak
   cp -R ../../app resources/app
   cd ..
@@ -64,7 +117,7 @@ build_linux_ia32 () {
 }
 
 build_linux_x64 () {
-  cd linux-x64
+  cd $LINUX_X64
   mv electron yakyak
   cp -R ../../app resources/app
   cd ..
@@ -73,7 +126,11 @@ build_linux_x64 () {
   cd ..
 }
 
-build_win32_ia32
-build_win32_x64
-build_linux_ia32
-build_linux_x64
+
+[[ $PLATFORMS =~ $DARWIN_X64 ]] && build_darwin_x64
+[[ $PLATFORMS =~ $WIN32_IA32 ]] && build_win32_ia32
+[[ $PLATFORMS =~ $WIN32_X64 ]] && build_win32_x64
+[[ $PLATFORMS =~ $LINUX_IA32 ]] && build_linux_ia32
+[[ $PLATFORMS =~ $LINUX_X64 ]] && build_linux_x64
+
+
